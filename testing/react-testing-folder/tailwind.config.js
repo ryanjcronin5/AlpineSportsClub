293f783764880b77ac74e1e1a23e6a01e5e4386d
Ryan Cronin <ryanjcronin5@gmail.com>
2023-03-21 12:25:34 +1300

    Week of 20/03/2023

---
+++ testing/react-testing-folder/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        fontFamily: {
+            'averia-sans-libre': ['Averia Sans Libre', 'san-serif']
+        },
+        extend: {},
+    },
+    plugins: [],
+}
