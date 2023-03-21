ed23f1371384c1830bf1f9da070a9e550824e939
Ryan Cronin <ryanjcronin5@gmail.com>
2023-03-22 11:04:05 +1300

    background working, plus text on home screen

--- testing/react-testing-folder/src/pages/parts/header.jsx
+++ testing/react-testing-folder/src/pages/parts/header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Header() {
     return (
-        <div className="flex flex-row h-screen bg-gray-100 p-3 w-screen">
-            <div className="bg-scroll bg-clip-border bg-origin-border bg-center bg-no-repeat bg-cover bg-header-image">
+        <div className="flex flex-col h-screen bg-gray-100 p-3 w-screen bg-header-image bg-no-repeat bg-center bg-local bg-clip-border bg-origin-border bg-cover">
+            <div className="flex flex-row">
                 <div>
                     <img src="logo.png" alt="logo" className="h-16"></img>
                 </div>
@@ -13,6 +13,8 @@ function Header() {
                     </h1>
                 </div>
             </div>
+            <div className="mt-96 p-3 rounded-lg bg-opacity-75 w-9/12 bg-gray-500 text-white">Alpine Sports Club Inc. (ASC) is a skiing/snowboarding, tramping, mountaineering, and outdoor recreation club based in Auckland New Zealand. ASC has 3 Huts available for members exclusive use during the Summer & Winter Seasons. Our very well-appointed bush Hut in the Waitakere Ranges, an hours drive from downtown Auckland. Located right up on the ridges of Whakapapa Ski Field are ASC's 2 Ski Huts; the A-Frame & Ruapehu Hut.</div>
+
         </div>
     );
 }
