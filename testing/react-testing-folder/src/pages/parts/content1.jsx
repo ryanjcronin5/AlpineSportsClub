1aa529bdf8ddb12eb71b87b5629df358fcceee59
Ryan Cronin <ryanjcronin5@gmail.com>
2023-03-27 17:59:11 +1300

    Sizing changes, hover change bg on navbar

--- testing/react-testing-folder/src/pages/parts/content1.jsx
+++ testing/react-testing-folder/src/pages/parts/content1.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Content1() {
     return (
-        <div className="flex flex-col w-full h-min">
+        <div className="flex flex-col w-auto h-auto">
             <img src="./img/image1.png" alt="Looking down snowy mountaions"></img>
-            <div className="p-3 rounded-lg bg-opacity-75 bg-gray-500 text-white">
-                <p>Alpine Sports Club Inc. (ASC) is a skiing/snowboarding, tramping, mountaineering, and outdoor recreation club based in Auckland New Zealand. ASC has 3 Huts available for members exclusive use during the Summer & Winter Seasons. Our very well-appointed bush Hut in the Waitakere Ranges, an hours drive from downtown Auckland. Located right up on the ridges of Whakapapa Ski Field are ASC's 2 Ski Huts; the A-Frame & Ruapehu Hut.</p>
+            <div className="py-3 px-5 rounded-lg bg-opacity-75 bg-gray-600 text-white text-justify mx-4">
+                <p className="align-middle">Alpine Sports Club Inc. (ASC) is a skiing/snowboarding, tramping, mountaineering, and outdoor recreation club based in Auckland New Zealand. ASC has 3 Huts available for members exclusive use during the Summer & Winter Seasons. Our very well-appointed bush Hut in the Waitakere Ranges, an hours drive from downtown Auckland. Located right up on the ridges of Whakapapa Ski Field are ASC's 2 Ski Huts; the A-Frame & Ruapehu Hut.</p>
             </div>
         </div>
     );
--- testing/react-testing-folder/src/pages/parts/sidenav.jsx
+++ testing/react-testing-folder/src/pages/parts/sidenav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SideNav() {
     return (
-        <div id="sidenav" className="flex flex-col h-screen p-3 bg-white shadow w-60">
+        <div id="sidenav" className="flex flex-col h-screen p-3 bg-white shadow w-1/4">
             <div className="space-y-3">
 
                 <div className="relative">
@@ -17,28 +17,28 @@ function SideNav() {
                 <div className="flex-1">
                     <ul className="pt-2 pb-4 space-y-1 text-sm">
 
-                        <li className="rounded-sm">
+                        <li className="rounded-sm hover:bg-slate-100">
                             <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
                                 <i className="fas fa-home text-2xl"></i>
                                 <span>Home</span>
                             </a>
                         </li>
 
-                        <li className="rounded-sm">
+                        <li className="rounded-sm hover:bg-slate-100">
                             <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
                                 <i className="fas fa-campground text-2xl"></i>
                                 <span>Activities & Huts</span>
                             </a>
                         </li>
 
-                        <li className="rounded-sm">
+                        <li className="rounded-sm hover:bg-slate-100">
                             <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
                                 <i className="fas fa-user-plus text-2xl"></i>
                                 <span>Join The Club</span>
                             </a>
                         </li>
 
-                        <li className="rounded-sm">
+                        <li className="rounded-sm hover:bg-slate-100">
                             <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
                                 <i className="fas fa-user-circle text-2xl"></i>
                                 <span>Log In</span>
