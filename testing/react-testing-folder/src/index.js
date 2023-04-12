ae629717ddc6408e6a86f7055e04d8ca22d88837
Ryan Cronin <ryanjcronin5@gmail.com>
2023-04-13 11:15:42 +1200

    Router fixes

--- testing/react-testing-folder/src/index.js
+++ testing/react-testing-folder/src/index.js
@@ -1,32 +1,39 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 
 import './index.css';
 import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 import SideNav from './pages/parts/sidenav';
 import NoPage from './pages/NoPage';
-import Act_Huts from './pages/parts/act_huts';
-import Join_Club from "./pages/parts/join_club";
-import Login from "./pages/parts/login";
+import Act_Huts from './pages/act_huts';
+import Join_Club from "./pages/join_club";
+import Login from "./pages/login";
 import * as React from "react";
 
-export default function App() {
+const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<SideNav />}>
-                    <Route index element={<Home />} />
-                    <Route path="act_huts" element={<Act_Huts />} />
-                    <Route path="join_club" element={<Join_Club />} />
-                    <Route path="login" element={<Login />} />
-
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
+            <div className="flex">
+                <SideNav /> {/* Render your Sidebar component */}
+                <div className="flex-1">
+                    {/* Use Outlet to render child routes */}
+                    <Routes>
+                        <Route path="/" element={<Outlet />}>
+                            <Route index element={<Home />} />
+                            <Route path="act_huts" element={<Act_Huts />} />
+                            <Route path="join_club" element={<Join_Club />} />
+                            <Route path="login" element={<Login />} />
+                            <Route path="*" element={<NoPage />} />
+                        </Route>
+                    </Routes>
+                </div>
+            </div>
         </BrowserRouter>
     );
-}
+};
+
+export default App;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
--- testing/react-testing-folder/src/pages/parts/sidenav.jsx
+++ testing/react-testing-folder/src/pages/parts/sidenav.jsx
@@ -2,26 +2,74 @@ import { Outlet, Link } from "react-router-dom";
 import { useState } from "react";
 
 function SideNav() {
-    const [isNavOpen, setIsOpen] = useState(false);
+    const [isNavOpen, setIsNavOpen] = useState(false);
     return (
         <>
-            <div id="sidenav" className="flex flex-col h-fill p-3 bg-white shadow w-1/3">
+            <div id="sidenav" className="flex flex-col h-fill p-3 bg-white shadow w-1/6">
                 <div className="space-y-3 fixed">
+                    <section className="MOBILE-MENU flex lg:hidden">
+                        <div className="HAMBURGER-ICON space-y-2" onClick={() => setIsNavOpen((prev) => !prev)}>
+                            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                        </div>
 
-                    {/* Search Bar */}
-                    <div className="relative">
-                        <span className="absolute inset-y-0 left-0 flex items-center py-4">
-                            <button type="submit" className="p-2 focus:outline-none focus:ring">
-                                <i className="fas fa-search text-2xl"></i>
-                            </button>
-                        </span>
-                        <input type="search" name="Search" placeholder="Search..." className="w-min py-2 pl-10 text-sm rounded-md focus:outline-none" />
-                    </div>
-
-                    <div className="flex-1">
-                        <ul className="pt-2 pb-4 space-y-1 text-sm">
-
-                            <Link to="/"> {/* Home */}
+                        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                            <div className="space-x-72">
+                                <div className="CROSS-ICON absolute top-0 right-0 px-1 py-1" onClick={() => setIsNavOpen(false)}>
+                                    <svg className="h-8 w-8 text-gray-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                        <line x1="18" y1="6" x2="6" y2="18" />
+                                        <line x1="6" y1="6" x2="18" y2="18" />
+                                    </svg>
+                                </div>
+                                <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-start space-y-6 justify-between min-h-[15rem] w-80">
+                                    <Link to="/">
+                                        <li className="rounded-sm hover:bg-slate-100">
+                                            <div className="flex items-center p-2 space-x-3 rounded-md">
+                                                <i className="fas fa-home text-4xl"></i>
+                                                <span className="text-3xl">Home</span>
+                                            </div>
+                                        </li>
+                                    </Link>
+                                    <Link to="/act_huts">
+                                        <li className="rounded-sm hover:bg-slate-100">
+                                            <div className="flex items-center p-2 space-x-3 rounded-md">
+                                                <i className="fas fa-campground text-4xl"></i>
+                                                <span className="text-3xl">Activities & Huts</span>
+                                            </div>
+                                        </li>
+                                    </Link>
+                                    <Link to="/join_club">
+                                        <li className="rounded-sm hover:bg-slate-100">
+                                            <div className="flex items-center p-2 space-x-3 rounded-md">
+                                                <i className="fas fa-user-plus text-4xl"></i>
+                                                <span className="text-3xl">Join The Club</span>
+                                            </div>
+                                        </li>
+                                    </Link>
+                                    <Link to="/login">
+                                        <li className="rounded-sm hover:bg-slate-100">
+                                            <div className="flex items-center p-2 space-x-3 rounded-md">
+                                                <i className="fas fa-user-circle text-4xl"></i>
+                                                <span className="text-3xl">Log In</span>
+                                            </div>
+                                        </li>
+                                    </Link>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                    <ul className="DESKTOP-MENU hidden lg:flex lg:flex-col">
+                        <div className="relative">
+                            <span className="absolute inset-y-0 left-0 flex items-center py-4">
+                                <button type="submit" className="p-2 focus:outline-none focus:ring">
+                                    <i className="fas fa-search text-2xl"></i>
+                                </button>
+                            </span>
+                            <input type="search" name="Search" placeholder="Search..." className="w-min py-2 pl-10 text-sm rounded-md focus:outline-none" />
+                        </div>
+                        <div className="flex-1">
+                            <Link to="/">
                                 <li className="rounded-sm hover:bg-slate-100">
                                     <div className="flex items-center p-2 space-x-3 rounded-md">
                                         <i className="fas fa-home text-2xl"></i>
@@ -29,8 +77,7 @@ function SideNav() {
                                     </div>
                                 </li>
                             </Link>
-
-                            <Link to="/act_huts"> {/* Activities & Huts */}
+                            <Link to="/act_huts">
                                 <li className="rounded-sm hover:bg-slate-100">
                                     <div className="flex items-center p-2 space-x-3 rounded-md">
                                         <i className="fas fa-campground text-2xl"></i>
@@ -38,8 +85,7 @@ function SideNav() {
                                     </div>
                                 </li>
                             </Link>
-
-                            <Link to="/join_club"> {/* Join the Club */}
+                            <Link to="/join_club">
                                 <li className="rounded-sm hover:bg-slate-100">
                                     <div className="flex items-center p-2 space-x-3 rounded-md">
                                         <i className="fas fa-user-plus text-2xl"></i>
@@ -47,8 +93,7 @@ function SideNav() {
                                     </div>
                                 </li>
                             </Link>
-
-                            <Link to="/login"> {/* Log In */}
+                            <Link to="/login">
                                 <li className="rounded-sm hover:bg-slate-100">
                                     <div className="flex items-center p-2 space-x-3 rounded-md">
                                         <i className="fas fa-user-circle text-2xl"></i>
@@ -56,9 +101,27 @@ function SideNav() {
                                     </div>
                                 </li>
                             </Link>
-
-                        </ul>
-                    </div>
+                        </div>
+                    </ul>
+                    <style>{`
+                        .hideMenuNav {
+                            display: none;
+                        }
+                        .showMenuNav {
+                            display: block;
+                            position: absolute;
+                            width: 100%;
+                            height: 100vh;
+                            top: 0;
+                            left: 0;
+                            background: white;
+                            z-index: 10;
+                            display: flex;
+                            flex-direction: column;
+                            justify-content: space-evenly;
+                            align-items: center;
+                        }
+                    `}</style>
                 </div>
             </div>
             <Outlet />
